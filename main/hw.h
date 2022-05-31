5cfa5c9c0dff5063748ceb5206f3fc03f6e59f9b
sheraqim shirxanloo <mshirxanloo@gmail.com>
2022-05-31 12:59:13 +0430

    1401/03/10  adc

--- main/hw.h
+++ main/hw.h
@@ -6,6 +6,20 @@ extern "C" {
 #endif
 
 
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "esp_log.h"
+
 
 
 #ifdef __cplusplus
--- main/main.c
+++ main/main.c
@@ -6,67 +6,38 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include <stdio.h>
-#include <stddef.h>
-
-#include "driver/gpio.h"
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-#include "esp_system.h"
-#include "esp_spi_flash.h"
 
 #include "hw.h"
 
-#define GPIO_LED 2
-#define GPIO_LED_PIN_SEL (1ULL << GPIO_LED)
-#define GPIO_BUTTON 5
-#define GPIO_BUTTON_PIN_SEL (1ULL << GPIO_BUTTON)
-#define ESP_INTR_FLAG_DEFAULT 0
 
-static void button_handler(void *arg);
-
-static void init_hw(void)
-{
-    gpio_config_t io_conf;
-
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = GPIO_LED_PIN_SEL;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.pull_down_en = 0;
-    io_conf.pull_up_en = 0;
-    gpio_config(&io_conf);
-
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = GPIO_BUTTON_PIN_SEL;
-    io_conf.intr_type = GPIO_INTR_NEGEDGE;
-    io_conf.pull_up_en = 1;
-    gpio_config(&io_conf);
-
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    gpio_isr_handler_add(GPIO_BUTTON, button_handler, NULL);
-}
-
-static TickType_t next = 0;
-static bool led_state = false;
-
-static void IRAM_ATTR button_handler(void *arg)
-{
-    TickType_t now = xTaskGetTickCountFromISR();
-    if (now > next)
-    {
-        led_state = !led_state;
-        gpio_set_level(GPIO_LED, led_state);
-        next = now + 500 / portTICK_PERIOD_MS;
+#include "my-adc.h"
+static void adc_task() {
+    int x;
+    uint16_t adc_data[100];
+    while(1) {
+        if(ESP_OK == adc_read(&adc_data[0])) {
+            ESP_LOGI(TAG, "adc read: %d\r\n", adc_data[0]);
+        }
+        ESP_LOGI(TAG, "adc read fast:\r\n");
+        if(ESP_OK == adc_read_fast(adc_data, 100)) {
+            for(x=0; x<100; x++) {
+                printf("%d\n", adc_data[x]);
+            }
+        }
+        vTaskDelay(1000/portTICK_RATE_MS);
     }
 }
 
+
+
 void app_main()
 {
-    init_hw();
+    initialize_adc();
+    //...................................................................
 
-    vTaskSuspend(NULL);
+
+    xTaskCreate(adc_task, "adc_task", 1024, NULL, 5, NULL);
 }
+
 
 
